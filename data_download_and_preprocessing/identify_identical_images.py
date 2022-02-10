398dd4f31409134f4eaebe54f83307bd3f1784c0
Celine Robinson <csr33@duke.edu>
2022-02-10 14:09:31 -0500

    iii

--- data_download_and_preprocessing/identify_identical_images.py
+++ data_download_and_preprocessing/identify_identical_images.py
@@ -33,11 +33,55 @@ def get_args_parse():
                         help='path to positive chips in complete dataset.')
     args = parser.parse_args()
     return args
+
+def list_of_lists_positive_chips(chips_positive_path):
+    positive_chips = os.listdir(chips_positive_path)
+    positive_chips_lists = [positive_chips[x:x+1000] for x in range(0, len(positive_chips), 1000)]
+    return(positive_chips_lists)
+
+def identify_identical_images(o_images_dir_path, d_images_dir_path, o_images = None):
+    """
+    Args:
+    images_dir_path(str): path to directory containing images of interest
+    Returns
+    same_images(list of lists): lists of images that contain that same information
+    https://pysource.com/2018/07/19/check-if-two-images-are-equal-with-opencv-and-python/
+    """                         
+    same_images_o_images = [] #Make a list to hold the identical images
+    same_images_d_images = [] #Make a list to hold the identical images
+         
+    #Make a list of the images to check for duplicates (images in directory or provided as arugment in function)
+    if o_images == None:
+        o_images = os.listdir(os.path.join(o_images_dir_path)) 
+    d_images = os.listdir(os.path.join(d_images_dir_path))
+
+    for o in tqdm.tqdm(range(len(o_images))):
+        o_image = o_images[o]
+        original = cv2.imread(os.path.join(o_images_dir_path, o_image)) #open image
+
+        for d_image in d_images:
+            duplicate = cv2.imread(os.path.join(d_images_dir_path, d_image)) #open image
+
+            #check for similar characteristics
+            if original.shape == duplicate.shape:
+                difference = cv2.subtract(original, duplicate)
+                b, g, r = cv2.split(difference)
+
+            if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+                if o_image != d_image:
+                    same_images_o_images.append([o_image]) #Make a list to hold the identical images
+                    same_images_d_images.append([d_image]) #Make a list to hold the identical images
+                    o_images.remove(d_image) #remove duplicate images, because you have already at least one version to use to find others
+        
+        d_images.remove(o_image) #remove o_image from d_images list, because you have already checked it against each image
+    
+    same_images = np.array(same_images_o_images,same_images_d_images)
+    return(same_images)
     
 def main(args): 
     fc.remove_thumbs(args.chips_positive_path)  
-    positive_chips_lists = fc.list_of_lists_positive_chips(args.chips_positive_path)
-    same_images = fc.identify_identical_images(args.chips_positive_path, args.chips_positive_path, positive_chips_lists[0])
+    positive_chips_lists = list_of_lists_positive_chips(args.chips_positive_path)
+    same_images = identify_identical_images(args.chips_positive_path, args.chips_positive_path, positive_chips_lists[0])
     np.save("same_images.npy", same_images)
     
 if __name__ == "__main__":
