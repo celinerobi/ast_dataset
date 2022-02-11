025137d5fbb3896bf5a560cc58068be71a53cef0
Celine Robinson <csr33@duke.edu>
2022-02-11 12:51:26 -0500

    iii

--- data_download_and_preprocessing/identify_identical_images.py
+++ data_download_and_preprocessing/identify_identical_images.py
@@ -31,25 +31,13 @@ def get_args_parse():
         description='This script records the annotator who has labeled each image')
     parser.add_argument('--chips_positive_path', type=str, default = "C:/chip_allocation/complete_dataset/chips_positive",
                         help='path to positive chips in complete dataset.')
+        parser.add_argument('--blocks', type=str, default = "20",
+                        help='path to positive chips in complete dataset.')
+        parser.add_argument('--block', type=str, default = "20",
+                        help='path to positive chips in complete dataset.')
     args = parser.parse_args()
     return args
-
-def remove_thumbs(path_to_folder_containing_images):
-    """ Remove Thumbs.db file from a given folder
-    Args: 
-    path_to_folder_containing_images(str): path to folder containing images
-    Returns:
-    None
-    """
-    if len(glob(path_to_folder_containing_images + "/*.db", recursive = True)) > 0:
-        os.remove(glob(path_to_folder_containing_images + "/*.db", recursive = True)[0])
-
-def list_of_lists_positive_chips(chips_positive_path,blocks):
-    positive_chips = os.listdir(chips_positive_path)
-    positive_chips_lists = [positive_chips[x:x+blocks] for x in range(0, len(positive_chips), 1000)]
-    return(positive_chips_lists)
-
-def identify_identical_images(o_images_dir_path, d_images_dir_path, o_images = None):
+def identify_identical_images(o_images_dir_path, d_images_dir_path, blocks, block):#o_images = None,):
     """
     Args:
     images_dir_path(str): path to directory containing images of interest
@@ -61,9 +49,11 @@ def identify_identical_images(o_images_dir_path, d_images_dir_path, o_images = N
     same_images_d_images = [] #Make a list to hold the identical images
          
     #Make a list of the images to check for duplicates (images in directory or provided as arugment in function)
-    if o_images == None:
-        o_images = os.listdir(os.path.join(o_images_dir_path)) 
+    #if o_images == None:
+    #    o_images = os.listdir(os.path.join(o_images_dir_path)) 
     d_images = os.listdir(os.path.join(d_images_dir_path))
+
+    o_images = [d_images[x:x+blocks] for x in range(0, len(d_images), blocks)][block]
 
     for o in tqdm.tqdm(range(len(o_images))):
         o_image = o_images[o]
@@ -81,7 +71,8 @@ def identify_identical_images(o_images_dir_path, d_images_dir_path, o_images = N
                 if o_image != d_image:
                     same_images_o_images.append([o_image]) #Make a list to hold the identical images
                     same_images_d_images.append([d_image]) #Make a list to hold the identical images
-                    o_images.remove(d_image) #remove duplicate images, because you have already at least one version to use to find others
+                    if d_image in o_images:
+                        o_images.remove(d_image) #remove duplicate images, because you have already at least one version to use to find others
         
         d_images.remove(o_image) #remove o_image from d_images list, because you have already checked it against each image
     
@@ -89,8 +80,8 @@ def identify_identical_images(o_images_dir_path, d_images_dir_path, o_images = N
     return(same_images)
     
 def main(args): 
-    remove_thumbs(args.chips_positive_path)  
     positive_chips_lists = list_of_lists_positive_chips(args.chips_positive_path,20)
+    np.load("load", same_images)
     same_images = identify_identical_images(args.chips_positive_path, args.chips_positive_path, positive_chips_lists[0])
     np.save("same_images.npy", same_images)
     
