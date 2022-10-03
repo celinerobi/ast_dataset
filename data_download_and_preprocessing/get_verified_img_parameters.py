1a37e7120c2941bc73264cd75200e77a14021e93
Celine Robinson <csr33@duke.edu>
2022-10-03 16:40:32 -0400

    make parm dir flag consistent

--- data_download_and_preprocessing/get_verified_img_parameters.py
+++ data_download_and_preprocessing/get_verified_img_parameters.py
@@ -38,20 +38,20 @@ def main(args):
     #state year
     all_verified_state_year_subfolders_path = ap.img_path_anno_path(ap.list_of_sub_directories(args.verified_state_year_subfolders_path)) 
     state_year_img_paths, state_year_xml_paths = fc.get_img_xml_paths(all_verified_state_year_subfolders_path)
-    fc.write_list(state_year_img_paths, os.path.join(args.params_directory,"state_year_img_paths.json"))
-    fc.write_list(state_year_xml_paths, os.path.join(args.params_directory,"state_year_xml_paths.json"))
+    fc.write_list(state_year_img_paths, os.path.join(args.param_directory,"state_year_img_paths.json"))
+    fc.write_list(state_year_xml_paths, os.path.join(args.param_directory,"state_year_xml_paths.json"))
     ##six digit idx
     state_year_six_digit_idx_list = fc.get_six_digit_index_from_img_path(state_year_img_paths)
-    fc.write_list(state_year_six_digit_idx_list, os.path.join(args.params_directory,"state_year_six_digit_idx_list.json"))
+    fc.write_list(state_year_six_digit_idx_list, os.path.join(args.param_directory,"state_year_six_digit_idx_list.json"))
     #standard quad
     all_verified_standard_quad_subfolders_path = ap.img_path_anno_path(ap.list_of_sub_directories(args.verified_standard_quad_subfolders_path)) 
     standard_img_paths, standard_xml_paths = fc.get_img_xml_paths(all_verified_standard_quad_subfolders_path)
-    fc.write_list(standard_img_paths, os.path.join(args.params_directory,"standard_img_paths.json"))
-    fc.write_list(standard_xml_paths, os.path.join(args.params_directory,"standard_xml_paths.json"))
+    fc.write_list(standard_img_paths, os.path.join(args.param_directory,"standard_img_paths.json"))
+    fc.write_list(standard_xml_paths, os.path.join(args.param_directory,"standard_xml_paths.json"))
     #yx indx
     ys, xs = fc.get_x_y_index(standard_img_paths)
     yx_list = np.vstack((ys, xs)).T
-    np.save(os.path.join(args.params_directory,"yx_list.npy"), yx_list)
+    np.save(os.path.join(args.param_directory,"yx_list.npy"), yx_list)
     
 if __name__ == '__main__':
     ### Get the arguments 
